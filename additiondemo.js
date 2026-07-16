0f170a39e9e8c044b87288a60e02b1b2d3c3d6a0
somwanshisiddhi <siddhisom2001@gmail.com>
2026-07-16 14:59:02 +0530

    js prompt

---
+++ additiondemo.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")
+
+();
+
+let num1 =Number(prompt("Enter first number:"));
+let num2 =Number(prompt("Enter second number:"));
+
+let sum=num1+num2;
+
+console.log("addition="+sum);
